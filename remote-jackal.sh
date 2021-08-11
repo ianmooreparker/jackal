96b196193d1ab05cf2e4d0866fbd1d8ba435958e
Ian Moore <ian.moore@parker.com>
2021-08-11 16:22:27 -0400

    upload from brick

---
+++ remote-jackal.sh
@@ -0,0 +1,7 @@
+#export ROS_MASTER_URI=http://cpr-j100-0309:11311 #jackal's hostname
+export ROS_MASTER_URI=http://192.168.1.201:11311 #jackal's hostname
+#export ROS_MASTER_URI=http://192.168.1.40:11311
+#export ROS_IP=192.168.0.140
+#export ROS_IP=192.168.1.65
+export ROS_IP=192.168.1.77
+#export ROS_IP=cpr-j100-0309
