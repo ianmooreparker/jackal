4948aa479ebc02b35cc7778373ff411bf950e1f8
david_robbins <david_robbins@lord.com>
2021-08-13 11:55:07 -0400

    updated IP address for T460

--- remote-jackal.sh
+++ remote-jackal.sh
@@ -3,5 +3,5 @@ export ROS_MASTER_URI=http://192.168.1.201:11311 #jackal's hostname
 #export ROS_MASTER_URI=http://192.168.1.40:11311
 #export ROS_IP=192.168.0.140
 #export ROS_IP=192.168.1.65
-export ROS_IP=192.168.1.77
+export ROS_IP=192.168.1.36
 #export ROS_IP=cpr-j100-0309
